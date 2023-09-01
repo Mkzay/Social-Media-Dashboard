3afc9e62f761b239ab03fe40f6d9e60292eb8c95
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-09-01 01:57:32 +0100

    Base

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*const checkbox = document.querySelector('input[type="checkbox"]');
-console.log(checkbox);
-
-checkbox.addEventListener('change', function(){
-    if (checkbox.checked) {
-        document.documentElement.classList.add('dark')
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
-});*/
-
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
